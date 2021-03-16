86f5157453b0b0f5fda6ccef6b63baa3b404bb72
aniballajoat <aniballajo.at@gmail.com>
2021-03-15 22:06:31 -0500

    primera-clase-js-json

---
+++ semana4/dia1/clase/script.js
@@ -0,0 +1,60 @@
+let nombre = "Anibal";
+let sistemaOperativo = "Windows";
+console.log(typeof nombre);
+//--------------------Primer codigo
+/*  
+
+alert(
+    "Mi nombre es " + nombre +
+    " y mi sistema operativo es " + sistemaOperativo
+);
+
+alert(`Mi nombre es ${nombre} y mi sistema operativo es ${sistemaOperativo}`);
+*/
+
+
+//--------------------BMI
+/*
+let altura = 1.74;
+let peso = 84.700;
+let bmi = (peso) / (altura **2);
+alert(bmi)
+*/
+
+//-------------------------operaciones
+
+/*
+let a = 6;
+let b = 10;
+
+let suma = a + b;
+console.log(suma);
+
+let resta = b - a;
+console.log(resta);
+
+let division = b / a;
+console.log(division);
+
+let multiplicacion = a * b;
+console.log(multiplicacion);
+
+let potencia = a ** b;
+console.log(potencia);
+
+let radicacion = a * 0.5;
+console.log(radicacion);
+
+let modulo = b % a;
+console.log(modulo);
+*/
+//--------farenheit = celsius formula
+
+let celsius = 1;
+let farenheit = (celsius * ( 9 / 5 ) + 32 );
+console.log(farenheit)
+
+//---------celsius = farenheit formila
+let farenheit2 = 32;
+let celsius2 = ((farenheit2 - 32 ) *  5 / 9  );
+console.log(celsius2);
---
+++ semana4/dia1/tarea/script.js
@@ -0,0 +1,28 @@
+//------------1------------------
+
+let misDatos = {
+    hobbie1: "dormir",
+    hobbie2: "dormirx2",
+    hobbie3: "dormirx3", 
+
+    numMascotas: 2,
+
+    hombre: true,
+    mujer: false
+
+};
+//------------2------------------
+let miNombre = prompt("Ingrese su nombre: ");
+alert(`Saludos ${miNombre}!!`);
+//------------3------------------
+let raiz1, raiz2;
+
+let a = prompt("ingrese a: ");
+let b = prompt("ingrese b: ");
+let c = prompt("ingrese c: ");
+
+
+raiz1 = (-b + (b * b - 4 * a * c)**0.5) / (2 * a);
+raiz2 = (-b - (b * b - 4 * a * c)**0.5) / (2 * a);
+
+console.log(`Los resultados de la formula son ${raiz1} y ${raiz2}`);
