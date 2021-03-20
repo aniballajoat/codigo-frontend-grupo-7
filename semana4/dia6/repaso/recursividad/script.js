cee757ad782aff709b25676204ae3636c0af5947
aniballajoat <aniballajo.at@gmail.com>
2021-03-20 12:26:37 -0500

    repaso-recursividad

---
+++ semana4/dia6/repaso/recursividad/script.js
@@ -0,0 +1,33 @@
+/*
+let mi_nombre = prompt("Adivina mi nombre?");
+while(mi_nombre.toLowerCase()!== "anibal"){
+    mi_nombre = prompt("Sigue intentando!!");
+}
+alert("Hey!!, adivinaste mi nombre");
+
+
+*/
+
+
+
+/*
+
+let mi_nombre = prompt("Adivina mi nombre?");
+function adivina(){
+    if (mi_nombre.toLowerCase()==="anibal"){
+        alert("Hey!!, adivinaste mi nombre.");
+    }
+    else{
+        mi_nombre = prompt("Sigue intentado!!");
+        adivina();
+    }
+}
+adivina();
+*/
+
+let mi_nombre = prompt("Adivina mi nombre?");
+
+do{
+    mi_nombre = prompt("Sigue intentando!!");
+}while(mi_nombre.toLowerCase()!== "anibal")
+alert("Hey!!, adivinaste mi nombre");
